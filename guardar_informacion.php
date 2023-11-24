6d36cc4be5a33505d1a182ac3d8a94f20561d2ff
MatiasRojasdechile <146135012+MatiasRojasdechile@users.noreply.github.com>
2023-11-24 02:21:56 +0000

    se arreglo el codigo

--- guardar_informacion.php
+++ guardar_informacion.php
@@ -1,23 +1,43 @@
 <?php
+include("db.php");
 
-include('db.php');
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['save_task'])) {
+    $title = $_POST['title'];
+    $objeto = $_POST['objeto'];
+    $description = $_POST['description'];
+    $correo = $_POST['correo'];
 
-if (isset($_POST['save_task'])) {
-  $title = $_POST['title'];
-  $objeto = $_POST['objeto'];
-  $description = $_POST['description'];
-  $imagen = $_POST['imagen'];
-  $correo = $_POST['correo'];
-  $query = "INSERT INTO task(title, objeto ,description, imagen, correo) VALUES ('$title','$objeto','$description','$imagen','$correo')";
-  $result = mysqli_query($conn, $query);
-  if(!$result) {
-    die("Query Failed.");
-  }
+    // Manejo del archivo
+    $imagen_link = '';  // Inicializa la variable de enlace de imagen
 
-  $_SESSION['message'] = 'Se guardo exitosamente';
-  $_SESSION['message_type'] = 'success';
-  header('Location: index.php');
+    if (isset($_FILES['imagen']) && $_FILES['imagen']['error'] === UPLOAD_ERR_OK) {
+        $target_dir = "imagen/";  // Directorio de carga 
+        $target_file = $target_dir . basename($_FILES["imagen"]["name"]);
+        
+        // Mueve el archivo al directorio de carga
+        if (move_uploaded_file($_FILES["imagen"]["tmp_name"], $target_file)) {
+            $imagen_link = $target_file;
+        } else {
+            $_SESSION['message'] = 'Error al cargar la imagen.';
+            $_SESSION['message_type'] = 'danger';
+            header('Location: index.php');
+            exit();
+        }
+    }
 
+    // Inserta los datos en la base de datos
+    $query = "INSERT INTO task (title, objeto, description, imagen, correo) VALUES ('$title', '$objeto', '$description', '$imagen_link', '$correo')";
+    $result = mysqli_query($conn, $query);
+
+    if (!$result) {
+        $_SESSION['message'] = 'Error al guardar la tarea.';
+        $_SESSION['message_type'] = 'danger';
+    } else {
+        $_SESSION['message'] = 'Tarea guardada exitosamente.';
+        $_SESSION['message_type'] = 'success';
+    }
+
+    header('Location: index.php');
+    exit();
 }
-
 ?>
