7edf90114fa6f1b77061e896d694f08381a270d5
MatiasRojasdechile <146135012+MatiasRojasdechile@users.noreply.github.com>
2023-12-10 22:55:05 +0000

    final

--- includes/header.php
+++ includes/header.php
@@ -1,18 +1,81 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <title>PHP CRUD MYSQL</title>
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <!-- BOOTSTRAP 4 -->
-    <link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css">
-    <!-- FONT AWESOEM -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-  </head>
-  <body>
+<head>
+  <meta charset="UTF-8">
+  <title>Objetos Perdidos</title>
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <!-- BOOTSTRAP 4 -->
+  <link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css">
+  <!-- FONT AWESOME -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
-    <nav class="navbar navbar-dark bg-dark">
-      <div class="container">
-        <a class="navbar-brand" href="index.php">PHP MySQL CRUD</a>
+  <style>
+  .navbar-orange {
+    background-color: rgb(255, 82, 0));
+  }
+
+  .navbar-brand,
+  .nav-link {
+    color: white !important;
+    font-size: 18px;
+  }
+
+  .navbar-brand:hover,
+  .nav-link:hover {
+    color: #ffe066 !important; /* Cambia el color al pasar el mouse */
+  }
+
+  .navbar-nav {
+    margin-right: 20px;
+  }
+
+  .navbar-logo {
+    margin-left: auto;
+  }
+  textarea {
+        resize: none;
+    }
+    .semicirculo-naranja {
+    position: relative;
+    overflow: hidden;
+    border-radius: 10px;
+  }
+</style>
+</head>
+<body>
+
+  <nav class="navbar navbar-dark navbar-orange">
+    <div class="container">
+      <div class="row w-100">
+        <!-- Sección del texto "objetos perdidos" -->
+        <div class="col-4">
+          <a class="navbar-brand" href="index.php">Objetos Perdidos</a>
+        </div>
+
+        <!-- Sección de los enlaces -->
+        <div class="col-4 d-flex justify-content-center">
+          <ul class="d-flex list-unstyled m-0">
+            <li class="nav-item mx-3">
+              <a class="nav-link" href="index.php">Registrar bici</a>
+            </li>
+            <li class="nav-item mx-3">
+              <a class="nav-link" href="tabla.php">Tabla</a>
+            </li>
+          </ul>
+        </div>
+
+        <!-- Sección de la imagen -->
+        <div class="col-4 d-flex justify-content-end mr-auto">
+          <a href="index.php">
+            <img src="logo/Logo_nuevo_ucen.png" width="15%" alt="">
+          </a>
+        </div>
       </div>
-    </nav>
+    </div>
+  </nav>
+  <div class="semicirculo-naranja p-4">
+
+  <!-- Tu contenido HTML continúa aquí... -->
+  
+</body>
+</html>
