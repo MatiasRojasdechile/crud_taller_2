73192b1fe6e519b4ca5b602ccbaf91952c6ce81d
MatiasRojasdechile <146135012+MatiasRojasdechile@users.noreply.github.com>
2023-12-10 22:46:46 +0000

    final

--- borrar_informacion.php
+++ borrar_informacion.php
@@ -2,17 +2,19 @@
 
 include("db.php");
 
-if(isset($_GET['id'])) {
-  $id = $_GET['id'];
-  $query = "DELETE FROM task WHERE id = $id";
-  $result = mysqli_query($conn, $query);
-  if(!$result) {
-    die("Query Failed.");
-  }
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
 
-  $_SESSION['message'] = 'Task Removed Successfully';
-  $_SESSION['message_type'] = 'danger';
-  header('Location: index.php');
+    // Modificar la consulta para actualizar el estado a 0 en lugar de eliminar
+    $query = "UPDATE task SET estado = 0 WHERE id = $id";
+    $result = mysqli_query($conn, $query);
+
+    if (!$result) {
+        die("Query Failed.");
+    }
+
+    $_SESSION['message_type'] = 'danger';
+    header('Location: tabla.php');
 }
 
 ?>
